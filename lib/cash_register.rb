7ff35119e48f058bc139296457471d367cfe5cd2
monginadiana <dianahmongina2@gmail.com>
2022-09-28 10:48:15 +0300

    commit tree 60f9897ad5e52a4fe2900b345929588c48225759

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require "pry"
+class CashRegister
+    attr_accessor :discount,:total,:items
+    def initialize(*discount)
+        @total=0
+        @discount=discount[0]
+        @items=[]
+    end
 
+    def total
+        @total
+    end
+    def total=(total)
+        @total+=total
+    end
+
+
+    def add_item(title,price,*quantity)
+        # binding.pry
+        if quantity!=[]
+        self.total=(price*quantity[0])
+       else
+           self.total=(price)
+       end
+    end
+
+    def apply_discount
+        if @discount.class.to_s=="Integer"
+        price_after_disc=@total*(100-@discount)/100
+        @total=price_after_disc
+        # binding.pry
+       "After the discount, the total comes to $#{@total}."
+        else
+        "There is no discount to apply."
+        end
+    end
+
+    def items(item,price,*args)
+        @items.push(item)
+        @items
+        # binding.pry
+    end
+    def void_last_transaction
+    
+    end
+end
